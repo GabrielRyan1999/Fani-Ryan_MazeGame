cc6912fbbdbe2487948aee0001e600f53d357b88
Kalistus Fani <Kalistus Fani@DESKTOP-LUDPP99>
2018-12-15 23:45:21 +0700

    0.4

--- Maze/src/model/Finish.java
+++ Maze/src/model/Finish.java
@@ -5,27 +5,51 @@
  */
 package model;
 
-
 /**
  *
  * @author Ryan
  */
-
-    public class Finish extends Sel {
+public class Finish extends Sel {
 
     private int lebar;
     private int tinggi;
     private char nilai;
 
     public Finish(int x, int y, int lebar, int tinggi, char nilai) {
-        super(x, y);      
+        super(x, y);
         this.lebar = lebar;
-        this.tinggi = tinggi;    
+        this.tinggi = tinggi;
         this.setNilai(nilai);
     }
 
-  
+    @Override
+    public int getLebar() {
+        return lebar;
+    }
+
+    @Override
+    public void setLebar(int lebar) {
+        this.lebar = lebar;
+    }
+
+    @Override
+    public int getTinggi() {
+        return tinggi;
+    }
+
+    @Override
+    public void setTinggi(int tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    @Override
+    public char getNilai() {
+        return nilai;
+    }
+
+    @Override
+    public void setNilai(char nilai) {
+        this.nilai = nilai;
+    }
 
 }
-
-
